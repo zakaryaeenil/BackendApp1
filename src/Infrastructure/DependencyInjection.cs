16d01868c303a9be358ade512e6bc7711b463be8
zakaryaeenil <elnilzakarya@gmail.com>
2024-12-24 20:55:31 +0100

    Configure Azure Developer Pipeline

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -145,7 +145,8 @@ public static class DependencyInjection
         {
             options.AddPolicy("AllowSpecificOrigins", policyBuilder =>
             {
-                policyBuilder.WithOrigins("http://localhost:4200","http://localhost:4300") // Add the Angular app's URL here
+                policyBuilder.WithOrigins("https://calm-beach-043458403.4.azurestaticapps.net",
+                        "http://localhost:4300") // Add the Angular app's URL here
                              .AllowAnyHeader()
                              .AllowAnyMethod()
                              .AllowCredentials();
